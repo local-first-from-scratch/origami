36f8916db629ee707d995cfbfc739b868139d40c
Brian Hicks <brian@brianthicks.com>
2025-05-13 14:13:52 -0500

    accept biome fixes

--- frontend/src/env.d.ts
+++ frontend/src/env.d.ts
@@ -1,10 +1,10 @@
 /// <reference types="@rsbuild/core/types" />
 
 declare module "*.vue" {
-	import type { DefineComponent } from "vue";
+  import type { DefineComponent } from "vue";
 
-	// biome-ignore lint/complexity/noBannedTypes: todo
-	// biome-ignore lint/suspicious/noExplicitAny: todo
-	const component: DefineComponent<{}, {}, any>;
-	export default component;
+  // biome-ignore lint/complexity/noBannedTypes: todo
+  // biome-ignore lint/suspicious/noExplicitAny: todo
+  const component: DefineComponent<{}, {}, any>;
+  export default component;
 }
