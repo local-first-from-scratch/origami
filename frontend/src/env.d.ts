c978d9b711b9db4153944401e393d97da9bd6012
Brian Hicks <brian@brianthicks.com>
2025-05-13 13:51:53 -0500

    add GitHub actions for CI

--- frontend/src/env.d.ts
+++ frontend/src/env.d.ts
@@ -1,9 +1,10 @@
 /// <reference types="@rsbuild/core/types" />
 
-declare module '*.vue' {
-  import type { DefineComponent } from 'vue';
+declare module "*.vue" {
+  import type { DefineComponent } from "vue";
 
-  // biome-ignore lint/complexity/noBannedTypes: reason
+  // biome-ignore lint/complexity/noBannedTypes: todo
+  // biome-ignore lint/suspicious/noExplicitAny: todo
   const component: DefineComponent<{}, {}, any>;
   export default component;
 }
