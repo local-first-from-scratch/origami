89e3d51b3f2400d1daaaab65e76b48ec089f7dbb
Brian Hicks <brian@brianthicks.com>
2025-05-16 06:54:02 -0500

    hook up subscriptions on the Vue side

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,34 +1,12 @@
 <script setup lang="ts">
 import * as store from "store";
+import { useDoc } from "./doc";
 
 store.dry_run();
+
+const doc = useDoc("12345");
 </script>
 
 <template>
-  <div class="content">
-    <h1>Rsbuild with Vue</h1>
-    <p>Start building amazing things with Rsbuild.</p>
-  </div>
+  {{doc}}
 </template>
-
-<style scoped>
-.content {
-  display: flex;
-  min-height: 100vh;
-  line-height: 1.1;
-  text-align: center;
-  flex-direction: column;
-  justify-content: center;
-}
-
-.content h1 {
-  font-size: 3.6rem;
-  font-weight: 700;
-}
-
-.content p {
-  font-size: 1.2rem;
-  font-weight: 400;
-  opacity: 0.5;
-}
-</style>
