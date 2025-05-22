9947e2faedccb21d87153a688fff35f2d8c09b8b
Brian Hicks <brian@brianthicks.com>
2025-05-22 06:51:38 -0500

    convert to real JS objects

--- store/src/document/value.rs
+++ store/src/document/value.rs
@@ -2,8 +2,7 @@ use std::collections::BTreeMap;
 
 #[derive(Debug, PartialOrd, PartialEq, Clone)]
 pub enum Value {
-    Int(isize),
-    Float(f64),
+    Number(f64),
     String(String),
     Bool(bool),
     Map(BTreeMap<String, Value>),
@@ -51,12 +50,61 @@ impl From<&str> for Value {
 
 impl From<f64> for Value {
     fn from(v: f64) -> Self {
-        Self::Float(v)
+        Self::Number(v)
     }
 }
 
-impl From<isize> for Value {
-    fn from(v: isize) -> Self {
-        Self::Int(v)
+impl From<i32> for Value {
+    fn from(v: i32) -> Self {
+        Self::Number(v.into())
+    }
+}
+
+impl From<u32> for Value {
+    fn from(v: u32) -> Self {
+        Self::Number(v.into())
+    }
+}
+
+#[wasm_bindgen::prelude::wasm_bindgen]
+extern "C" {
+    type ObjectExt;
+
+    #[wasm_bindgen(method, indexing_setter)]
+    fn set(this: &ObjectExt, key: js_sys::JsString, value: wasm_bindgen::JsValue);
+}
+
+// Implementation note: keep `use`s and similar from leaking outside this
+// implementation so we can put it behind a flag eventually for server
+// compilation.
+impl From<Value> for wasm_bindgen::JsValue {
+    fn from(value: Value) -> Self {
+        use js_sys::{Array, Object};
+        use wasm_bindgen::{JsCast, JsValue};
+
+        match value {
+            Value::Number(i) => JsValue::from_f64(i),
+            Value::String(s) => JsValue::from_str(&s),
+            Value::Bool(b) => JsValue::from_bool(b),
+            Value::Map(map) => {
+                let out = Object::new();
+
+                for (k, v) in map {
+                    out.unchecked_ref::<ObjectExt>().set(k.into(), v.into())
+                }
+
+                out.into()
+            }
+            Value::List(values) => {
+                let out = Array::new();
+
+                for v in values {
+                    out.push(&v.into());
+                }
+
+                out.into()
+            }
+            Value::Null => JsValue::null(),
+        }
     }
 }
