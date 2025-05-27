8d22f71a065e6e119c4e37c231c86236dc5094e7
Brian Hicks <brian@brianthicks.com>
2025-05-27 07:21:52 -0500

    make a second subscription as a test

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -9,10 +9,14 @@ const doc = watch(hub.create(store.RootKind.Map));
 setInterval(() => {
   doc.value.set("x", new Date().getTime());
 }, 1000);
+
+const doc2 = watch(hub.lookup(doc.value.documentId));
 </script>
 
 <template>
   Doc: {{doc}}
 
   Current value: {{doc.current()}}
+
+  Other: {{doc2.current()}}
 </template>
