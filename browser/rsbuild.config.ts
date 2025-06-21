702260c2cf207f67ca102fd72f34046cc3cfcbac
Brian Hicks <brian@brianthicks.com>
2025-06-21 13:01:16 -0500

    wire into the browser

--- browser/rsbuild.config.ts
+++ browser/rsbuild.config.ts
@@ -1,6 +1,17 @@
 import { defineConfig } from '@rsbuild/core';
 import { pluginVue } from '@rsbuild/plugin-vue';
+import { pluginWasmPack } from 'rsbuild-plugin-wasmpack';
 
 export default defineConfig({
-  plugins: [pluginVue()],
+  plugins: [
+    pluginVue(),
+    pluginWasmPack({
+      crates: [
+        {
+          path: '../store', // The path to your Rust crate
+          target: 'bundler', // The target environment (e.g., 'web', 'nodejs')
+        },
+      ],
+    }),
+  ],
 });
--- browser/src/App.vue
+++ browser/src/App.vue
@@ -1,28 +1,33 @@
+<script setup type="ts">
+import { greet } from 'store';
+</script>
+
 <template>
-  <div class="content">
-    <h1>Rsbuild with Vue</h1>
-    <p>Start building amazing things with Rsbuild.</p>
-  </div>
+    <div class="content">
+        <h1>Rsbuild with Vue</h1>
+        <p>Start building amazing things with Rsbuild.</p>
+        {{ greet("Brian") }}
+    </div>
 </template>
 
 <style scoped>
 .content {
-  display: flex;
-  min-height: 100vh;
-  line-height: 1.1;
-  text-align: center;
-  flex-direction: column;
-  justify-content: center;
+    display: flex;
+    min-height: 100vh;
+    line-height: 1.1;
+    text-align: center;
+    flex-direction: column;
+    justify-content: center;
 }
 
 .content h1 {
-  font-size: 3.6rem;
-  font-weight: 700;
+    font-size: 3.6rem;
+    font-weight: 700;
 }
 
 .content p {
-  font-size: 1.2rem;
-  font-weight: 400;
-  opacity: 0.5;
+    font-size: 1.2rem;
+    font-weight: 400;
+    opacity: 0.5;
 }
 </style>
