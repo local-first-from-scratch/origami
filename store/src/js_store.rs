0aea20e8372071fc8987e06ad6d76a5cbc82fea2
Brian Hicks <brian@brianthicks.com>
2025-07-11 07:03:32 -0500

    adjust js store to be callable again

--- store/src/js_store.rs
+++ store/src/js_store.rs
@@ -1,5 +1,5 @@
 use crate::storage::idb::{self, IDBStorage};
-use crate::store::{self, Store};
+use crate::store::{self, Store as GenericStore};
 use migrate::{Migration, Migrator};
 use std::collections::BTreeMap;
 use wasm_bindgen::prelude::*;
@@ -13,16 +13,16 @@ export type TypeMap = Record<string, any>;
 export function store<T extends TypeMap>(schemas: Record<keyof T, string>, migrations: any[]): Promise<Store<T>>;
 
 export class Store<T extends TypeMap> {
-  insert<K extends keyof T>(table: K, id: string, data: T[K]): Promise<void>;
+  insert<K extends keyof T>(table: K, data: T[K]): Promise<void>;
   list<K extends keyof T>(table: K): Promise<T[K][]>;
   // get<K extends keyof T>(table: K, id: string): Promise<T[K]>;
   // update<K extends keyof T>(table: K, id: String, updater: (current: T[K]) => void): void;
 }
 "#;
 
-#[wasm_bindgen(skip_typescript, js_name = "Store")]
-pub struct JsStore {
-    store: Store<IDBStorage>,
+#[wasm_bindgen(skip_typescript)]
+pub struct Store {
+    store: GenericStore<IDBStorage>,
 }
 
 /// Technical reason this is separate: store initialization needs to be done asynchronously
@@ -30,44 +30,47 @@ pub struct JsStore {
 /// that in the object's constructor would make things look really weird on the
 /// JavaScript side.
 #[wasm_bindgen(skip_typescript)]
-pub async fn store(schemas: JsValue, migrations_raw: JsValue) -> Result<JsStore, Error> {
+pub async fn store(schemas: JsValue, migrations_raw: JsValue) -> Result<Store, Error> {
+    console_error_panic_hook::set_once();
+
     let mut migrator = Migrator::new();
-    let migrations: Vec<Migration> = serde_wasm_bindgen::from_value(migrations_raw)?;
+    let migrations: Vec<Migration> =
+        serde_wasm_bindgen::from_value(migrations_raw).map_err(Error::Migration)?;
     for migration in migrations {
         migrator.add_migration(migration);
     }
 
-    Ok(JsStore::new(
+    Ok(Store::new(
         migrator,
-        serde_wasm_bindgen::from_value(schemas)?,
+        serde_wasm_bindgen::from_value(schemas).map_err(Error::SchemaMapping)?,
         IDBStorage::init().await?,
     ))
 }
 
-impl JsStore {
+impl Store {
     pub fn new(migrator: Migrator, schemas: BTreeMap<String, String>, storage: IDBStorage) -> Self {
-        JsStore {
-            store: Store::new(migrator, schemas, storage),
+        Store {
+            store: GenericStore::new(migrator, schemas, storage),
         }
     }
 }
 
 #[wasm_bindgen]
-impl JsStore {
-    #[wasm_bindgen(js_name = "insert")]
-    pub async fn js_insert(
-        &mut self,
-        table_js: JsString,
-        data: JsValue,
-    ) -> Result<JsString, Error> {
+impl Store {
+    #[wasm_bindgen]
+    pub async fn insert(&mut self, table_js: JsString, data: JsValue) -> Result<JsString, Error> {
         Ok(self
             .store
-            .insert(table_js.into(), serde_wasm_bindgen::from_value(data)?)
+            .insert(
+                table_js.into(),
+                serde_wasm_bindgen::from_value(data).map_err(Error::Value)?,
+            )
             .await?
             .to_string()
             .into())
     }
 
+    #[wasm_bindgen]
     pub async fn list(&self, _table_js: JsString) -> Result<JsValue, Error> {
         // let table: String = table_js.into();
         // let rows = self.storage.get_rows(&table).await?;
@@ -79,8 +82,12 @@ impl JsStore {
 
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
-    #[error("Invalid schema. Schemas must be an object with string keys and values.")]
-    Serde(#[from] serde_wasm_bindgen::Error),
+    #[error("Invalid migration. Details: {0}")]
+    Migration(serde_wasm_bindgen::Error),
+    #[error("Invalid schema mapping. Details: {0}")]
+    SchemaMapping(serde_wasm_bindgen::Error),
+    #[error("Invalid value. Details: {0}")]
+    Value(serde_wasm_bindgen::Error),
     #[error("IndexedDB error: {0}")]
     Idb(#[from] idb::Error),
     #[error("Store error: {0}")]
